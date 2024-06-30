6dd934608e4539988ff5d51666dbac1e914d3e9d
Vijaya Malla <mallabiju@gmail.com>
2024-06-30 21:35:28 +0545

    First Commit

---
+++ A_HelloNepal_Notepad/hello_nepal.java
@@ -0,0 +1,7 @@
+public class hello_nepal
+{
+	public static void main(String[] args)
+		{
+			System.out.println("Hello Nepal");
+		}
+}
---
+++ B_Basic_IO/a_hellonepal.java
@@ -0,0 +1,8 @@
+package B_Basic_IO;
+
+public class a_hellonepal {
+    public static void main(String[] args) {
+        System.out.println("Hello Nepal");
+    }
+}
+
