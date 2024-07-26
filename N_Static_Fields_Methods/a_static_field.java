653078a1b36cdc8024d2194e3a2ce1fc2decc1c7
Vijaya Malla <mallabiju@gmail.com>
2024-07-26 08:47:19 +0545

    Static Fields

---
+++ N_Static_Fields_Methods/a_static_field.java
@@ -0,0 +1,36 @@
+package N_Static_Fields_Methods;
+
+class Student {
+    int roll;
+    String name;
+    static String clzName = "Sanothimi";
+
+    void input(int r, String n) {
+        roll = r;
+        name = n;
+    }
+
+    void output() {
+        System.out.println("\nRoll = " + roll);
+        System.out.println("Name =" + name);
+        System.out.println("College name = " + clzName);
+    }
+}
+
+public class a_static_field {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+
+        s1.input(11, "Ram");
+        s2.input(12, "Sita");
+        s3.input(13, "Hari");
+
+        s1.output();
+        s2.output();
+        
+        Student.clzName = "Bhaktapur";
+        s3.output();
+    }
+}
---
+++ N_Static_Fields_Methods/a_static_variable.java
@@ -0,0 +1,11 @@
+package N_Static_Fields_Methods;
+
+public class a_static_variable {
+    static int roll = 78;
+    static String name = "Ram";
+
+    public static void main(String[] args) {
+        System.out.println(roll);
+        System.out.println(name);
+    }
+}
