2c9d550cccc40013ac297c6c775968c1b5921e3e
Vijaya Malla <mallabiju@gmail.com>
2024-08-15 13:53:36 +0545

    Exception handling

---
+++ X_Exception_Handling/a_exception_problem_scenario.java
@@ -0,0 +1,10 @@
+// this example includes problem statement, i.e. we cannot divide numbers by zero
+package X_Exception_Handling;
+
+public class a_exception_problem_scenario {
+    public static void main(String[] args) {
+        int a = 12, b = 0, result;
+        result = a / b;
+        System.out.println("The result = " + result);
+    }
+}
